da7b04915b1e64a769ac1f146b3b9488defd8efa
mehmetveyselsekendiz <mehmetveyselsekendiz@gmail.com>
2020-12-01 17:53:57 +0300

    Magic 8 Ball flutter app

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,39 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: Scaffold(
+          backgroundColor: Colors.blue.shade400,
+          appBar: AppBar(
+            backgroundColor: Colors.blue.shade800,
+            title: Center(
+              child: Text('Ask Me Anything'),
+            ),
+          ),
+          body: Answers(),
+        ),
       ),
     );
+
+class Answers extends StatefulWidget {
+  @override
+  _AnswersState createState() => _AnswersState();
+}
+
+class _AnswersState extends State<Answers> {
+  int count = 1;
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: FlatButton(
+        onPressed: () {
+          setState(() {
+            count = Random().nextInt(5) + 1;
+          });
+        },
+        child: Image.asset('./images/ball$count.png'),
+      ),
+    );
+  }
+}
